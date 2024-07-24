85790de93ad4c96a0aa03136206ca8f809887923
Nghia Pham <nghia.pq77@gmail.com>
2024-07-24 08:18:01 +0700

    Modify config and add an install script.

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -17,16 +17,16 @@
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+#define BLOCKS(X)                   \
+    X("", "sb-internet", 10, 10)    \
+    X("", "sb-bluetooth", 60, 9)    \
+    X("", "sb-ram", 3, 8)           \
+    X("", "sb-cpu", 3, 7)           \
+    X("", "sb-mailbox", 0, 6)       \
+    X("", "sb-lang", 0, 5)          \
+    X("", "sb-sound", 0, 4)         \
+    X("", "sb-battery", 30, 3)      \
+    X("", "sb-clock", 60, 2)        \
+    X("", "sb-tux", 0, 1)
 
 #endif  // CONFIG_H
